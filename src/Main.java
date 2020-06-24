5b2af28b78022264b0b49b8110ec1ced7a330d97
Ata <apir@shapesecurity.com>
2020-06-23 17:53:08 -0700

    done with exercise

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What is my age";
+        String choiceOne = "34";
+        String choiceTwo = "35";
+        String choiceThree = "45";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println("Please type either " + choiceOne + " " + choiceTwo + " or "+ choiceThree);
         // Have the user input an answer
         // Retrieve the user's input
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
-
+        if(input.equals(correctAnswer)){
+            System.out.println("Congratulations. You guessed correct.");
+        }
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
+        else{
+            System.out.println("Sorry your answer is incorrect. The correct answer is "+ correctAnswer);
+        }
     }
 
 }
